0f4c4e751c3bf045b40c8d6e5501341d373f87da
mecrym <maria.ecr.moreira1@gmail.com>
2026-04-04 18:16:27 -0300

    feat(cliente): implement addComputador method and update calculaTotalCompra

--- pcmania/src/main/java/pcmania/cliente/Cliente.java
+++ pcmania/src/main/java/pcmania/cliente/Cliente.java
@@ -1,12 +1,16 @@
 package pcmania.cliente;
 
+import pcmania.computador.Computador;
+
 public class Cliente {
     private String nome;
     private String cpf;
+    private Computador[] computadores;
 
     public Cliente(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
+        this.computadores = new Computador[10];
     }
 
     public String getNome() {
@@ -16,6 +20,17 @@ public class Cliente {
         return cpf;
     }
 
-    public float calculaTotalCompra(){}
+    public void addComputador(Computador computador, int indice){
+        this.computadores[indice] = computador;
+    }
 
+    public float calculaTotalCompra(){
+        float total = 0;
+        for(Computador computador : computadores){
+            if(computador != null){
+                total += computador.getPreco();
+            }
+        }
+        return total;
+    }
 }
