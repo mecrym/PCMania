b83a0a56315b80e9b5ee97979912e03483ae9033
mecrym <maria.ecr.moreira1@gmail.com>
2026-04-04 19:03:35 -0300

    fix(cliente): fix cliente and computador for the 2..* association

--- pcmania/src/main/java/pcmania/Main.java
+++ pcmania/src/main/java/pcmania/Main.java
@@ -1,7 +1,22 @@
 package pcmania;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite seu CPF: ");
+        String cpf = scanner.nextLine();
+
+        int matricula = 710;
+
+        Cliente cliente = new Cliente(nome, cpf);
+
+        int codigo;
+        int indice = 0;
+        
     }
 }
--- pcmania/src/main/java/pcmania/cliente/Cliente.java
+++ pcmania/src/main/java/pcmania/cliente/Cliente.java
@@ -6,11 +6,16 @@ public class Cliente {
     private String nome;
     private String cpf;
     private Computador[] computadores;
+    private int quantidadeComputadores;
 
-    public Cliente(String nome, String cpf) {
+    public Cliente(String nome, String cpf, int capacidade) {
         this.nome = nome;
         this.cpf = cpf;
-        this.computadores = new Computador[10];
+        if(capacidade < 0) {
+            capacidade = 0;
+        }
+        this.computadores = new Computador[capacidade];
+        this.quantidadeComputadores = 0;
     }
 
     public String getNome() {
@@ -19,17 +24,28 @@ public class Cliente {
     public String getCpf() {
         return cpf;
     }
+    public Computador[] getComputadores(){
+        return computadores;
+    }
+    public int getQuantidade(){
+        return quantidadeComputadores;
+    }
 
-    public void addComputador(Computador computador, int indice){
-        this.computadores[indice] = computador;
+    public void addComputador(Computador computador){
+        if (quantidadeComputadores >= computadores.length) {
+            Computador[] novoComputador = new Computador[computadores.length * 2];
+            for(int i = 0; i <  computadores.length; i++){
+                novoComputador[i] = computadores[i];
+            }
+            computadores = novoComputador;
+        }
+        computadores[quantidadeComputadores++] = computador;
     }
 
     public float calculaTotalCompra(){
         float total = 0;
-        for(Computador computador : computadores){
-            if(computador != null){
-                total += computador.getPreco();
-            }
+        for(int i=0; i<quantidadeComputadores; i++){
+            total +=computadores[i].getPreco();
         }
         return total;
     }
