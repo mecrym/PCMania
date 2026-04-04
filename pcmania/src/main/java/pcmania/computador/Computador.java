15ea5d6b51eeea8fbecc4cce08c700a4c2008283
mecrym <maria.ecr.moreira1@gmail.com>
2026-04-04 10:11:29 -0300

    feat(computador): add computador constructor and the compisition)

--- pcmania/src/main/java/pcmania/computador/Computador.java
+++ pcmania/src/main/java/pcmania/computador/Computador.java
@@ -1,9 +1,29 @@
 package pcmania.computador;
 
+import pcmania.hardware.HardwareBasico;
+import pcmania.hardware.MemoriaUSB;
+import pcmania.sistema.SistemaOperacional;
+
 public class Computador {
     private String marca;
     private float preco;
-    //Computados é a parte Todo
+    //Computador é a parte Todo, bo pras associações, yay...
+    private MemoriaUSB memoriaUSB;
+    private SistemaOperacional sistemaOperacional;
+    private HardwareBasico[] hardware;
+
+    public Computador(String marcaComputador, float precoComputador, String processadorNome, float processadorFrequencia, String memoriaRamNome, float memoriaRamCapacidade, String hdNome, float hdCapacidade) {
+        //comp
+        this.marca = marcaComputador;
+        this.preco = precoComputador;
+        //so é opcional
+        this.sistemaOperacional = null;
+        //eis a composição...
+        this.hardware = new HardwareBasico[3];
+        this.hardware[0] = new HardwareBasico(processadorNome, processadorFrequencia);
+        this.hardware[1] = new HardwareBasico(memoriaRamNome, memoriaRamCapacidade);
+        this.hardware[2] = new HardwareBasico(hdNome, hdCapacidade);
+    }
 
     public String getMarca() {
         return marca;
