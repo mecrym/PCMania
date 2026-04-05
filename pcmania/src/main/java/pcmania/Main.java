d8d6a130f34b59de11fee0864d7d9365a6c50c7d
mecrym <maria.ecr.moreira1@gmail.com>
2026-04-04 23:05:37 -0300

    feat(Main): refactor order processing and improve promotion handling

--- pcmania/src/main/java/pcmania/Main.java
+++ pcmania/src/main/java/pcmania/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import pcmania.cliente.Cliente;
 import pcmania.computador.Computador;
 import pcmania.hardware.MemoriaUSB;
-import pcmania.sistema.SistemaOperacional;
+import pcmania.utils.ProcessarPedidos;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,19 +17,35 @@ public class Main {
         String cpf = scanner.nextLine();
 
         int matricula = 710;
-        int capacidadeInicial = 10;
         int MIN_COMPUTADORES = 2;
         
-        Cliente cliente = new Cliente(nome, cpf, capacidadeInicial);
+        Cliente cliente = new Cliente(nome, cpf);
+        
+        Computador promocao1 = new Computador(
+            "Apple", matricula, "Pentium core i3", 2200, "Memória RAM", 8, "HD", 500
+        );
+        promocao1.addSistemaOperacional("manOS Sequoia", 64);
+        promocao1.addMemoriaUSB(new MemoriaUSB("Pen-drive", 16));
+        Computador.registraPromocao(promocao1);
+
+        Computador promocao2 = new Computador("Samsung", matricula + 1234, "Pentium core i5", 3370, "Memória Ram", 16, "HD", 1000);
+        promocao2.addSistemaOperacional("Windows 8", 64);
+        promocao2.addMemoriaUSB(new MemoriaUSB("Pen-drive", 32));
+        Computador.registraPromocao(promocao2);
+
+        Computador promocao3 = new Computador("Dell", matricula + 5678, "Pentium core i7", 4500, "Memória RAM", 32, "HD", 2000);
+        promocao3.addSistemaOperacional("Windows 10", 64);
+        promocao3.addMemoriaUSB(new MemoriaUSB("HD Externo", 1000));
+        Computador.registraPromocao(promocao3);
 
         int codigo;
-        
-        do{
-            System.out.println("Olá, bem vindo ao PCMania! Temos exelentes promocoes, confira:");
-            mostraPCConfigs();
-            System.out.println("Caso queira comprar um de nossos computadores, saiba que nossas promoções são validas apenas para as compras que contenham no mínimo " + MIN_COMPUTADORES + " computadores por cliente.");
-            System.out.println("Deseja comprar? Digite o código do computador para adiciona-lo, sendo 1 para a promoção 1, 2 para a promoção 2 e 3 para a promoção 3. Digite 0 para finalizar a compra.");
-            
+
+        do {
+            System.out.println("Olá, bem vindo ao PCMania! Temos excelentes promoções, confira:");
+            Computador.mostraPCConfigs();
+            System.out.println("Caso queira comprar um de nossos computadores, saiba que nossas promoções são válidas apenas para as compras que contenham no mínimo " + MIN_COMPUTADORES + " computadores por cliente.");
+            System.out.println("Deseja comprar? Digite o código do computador para adicioná-lo, sendo 1 para a promoção 1, 2 para a promoção 2 e 3 para a promoção 3. Digite 0 para finalizar a compra.");
+
             while (!scanner.hasNextInt()) {
                 System.out.println("Por favor, digite um número inteiro válido.");
                 scanner.next();
@@ -37,41 +53,42 @@ public class Main {
             codigo = scanner.nextInt();
             scanner.nextLine();
 
-            if(codigo == 1){
-                Computador pc = new Computador(
-                    "Apple", matricula, "Pentium Core i3", 2200, "Mémoria RAM", 8, "HD", 500 
-                );
-                pc.addSistemaOperacional(new SistemaOperacional("macOS Sequoia", 64));
-                pc.addMemoriaUSB(new MemoriaUSB("Pen-drive", 16));
-                cliente.addComputador(pc);
-
+            if (codigo == 1) {
+                cliente.addComputador(promocao1);
                 System.out.println("Promoção 1 adicionada!");
-            }else if(codigo == 2){
-                Computador pc = new Computador(
-                    "Samsung", matricula +1234, "Pentium Core i5", 3370, "Memória RAM", 16, "HD", 1000
-                );
-                pc.addSistemaOperacional(new SistemaOperacional("Windowns 8", 64));
-                pc.addMemoriaUSB(new MemoriaUSB("Pen-drive", 32));
-                cliente.addComputador(pc);
+            } else if (codigo == 2) {
+                cliente.addComputador(promocao2);
                 System.out.println("Promoção 2 adicionada!");
-            }else if(codigo == 3){
-                Computador pc = new Computador(
-                    "Dell", matricula+5678, "Pentium Core i7", 4500, "Memória RAM", 32, "HB", 2000
-                );
-                pc.addSistemaOperacional(new SistemaOperacional("Windows 10", 64));
-                pc.addMemoriaUSB(new MemoriaUSB("HD Externo", 1000));
-                cliente.addComputador(pc);
+            } else if (codigo == 3) {
+                cliente.addComputador(promocao3);
                 System.out.println("Promoção 3 adicionada!");
-            }else if (codigo == 0){
-                if(cliente.getQuantidade() < MIN_COMPUTADORES){
-                    System.out.println("Desculpe, mas você precisa comprar no mínimo "+ MIN_COMPUTADORES+" computadores...");
+            } else if (codigo == 0) {
+                if (cliente.getQuantidade() < MIN_COMPUTADORES) {
+                    System.out.println("Desculpe, mas você precisa comprar no mínimo " + MIN_COMPUTADORES + " computadores...");
                     System.out.println("Escolha mais um ítem da promoção antes de finalizar!");
-                }else{
+                } else {
                     break;
                 }
-            }else{
+            } else {
                 System.out.println("Código inválido. Digite 1, 2, 3 para selecionar sua promoção.");
             }
-    }while(true);
-    
+        } while (true);
+
+        System.out.println("========== RESUMO DA COMPRA ==========");
+        System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("CPF: " + cliente.getCpf());
+        System.out.println();
+
+        System.out.println("Computadores adquiridos:");
+        for (int i = 0; i < cliente.getQuantidade(); i++) {
+            System.out.println("--- PC " + (i + 1) + " ---");
+            cliente.getComputadores()[i].mostraDetalhesPromocoes();
+        }
+
+        System.out.println("Total da compra: R$" + cliente.calculaTotalCompra());
+
+        ProcessarPedidos.processar(cliente.getComputadores());
+
+        scanner.close();
+    }
 }
